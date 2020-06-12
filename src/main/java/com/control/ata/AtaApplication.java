6185f5e20c01ecdedcb98f73cd89b3822fc4b91f
Theodoros16 <theodoros16@gamil.com>
2020-06-12 20:21:38 -0300

    EnderecoDAO agora esta funcionando

--- src/main/java/com/control/ata/AtaApplication.java
+++ src/main/java/com/control/ata/AtaApplication.java
@@ -3,12 +3,15 @@ package com.control.ata;
 import com.control.ata.dao.EnderecoDAO;
 import com.control.ata.model.endereco.*;
 import org.directwebremoting.spring.DwrSpringServlet;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
+
+import java.util.ArrayList;
 
 @ImportResource(locations = "classpath:dwr-spring.xml")
 @SpringBootApplication
@@ -18,17 +21,25 @@ public class AtaApplication implements CommandLineRunner {
         SpringApplication.run(AtaApplication.class, args);
     }
 
+    @Autowired
+    private EnderecoDAO enderecoDAO;
+
     @Override
     public void run(String... args) throws Exception {
 
         Endereco e = new Endereco("rua", new Bairro("bairro", new
                 Cidade("cidade", new Estado("estado", new Pais("pais")))));
-        EnderecoDAO dao = new EnderecoDAO() {
-            @Override
-            public void run(String... args) throws Exception {
-                this.save(e);
-            }
-        };
+
+        ArrayList<Endereco> enderecos = new ArrayList<>();
+        enderecos.add(e);
+        enderecos.add(new Endereco("rua1", new Bairro("bairro1", new
+                Cidade("cidade1", new Estado("estado2", new Pais("pais3"))))));
+        enderecos.add(new Endereco("rua2", new Bairro("bairro1", new
+                Cidade("cidade1", new Estado("estado2", new Pais("pais3"))))));
+        enderecos.add(new Endereco("rua3", new Bairro("bairro1", new
+                Cidade("cidade1", new Estado("estado2", new Pais("pais3"))))));
+
+        enderecoDAO.saveAll(enderecos);
 
     }
 
--- src/main/java/com/control/ata/dao/EnderecoDAO.java
+++ src/main/java/com/control/ata/dao/EnderecoDAO.java
@@ -3,9 +3,10 @@ package com.control.ata.dao;
 import com.control.ata.model.endereco.Endereco;
 import com.control.ata.repository.endereco.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
 
-public abstract class EnderecoDAO implements CommandLineRunner {
+@Service
+public class EnderecoDAO {
 
     @Autowired
     private PaisRepository paisRepository;
@@ -24,6 +25,12 @@ public abstract class EnderecoDAO implements CommandLineRunner {
         cidadeRepository.save(endereco.getBairro().getCidade());
         bairroRepository.save(endereco.getBairro());
         enderecoRepository.save(endereco);
+    }
+
+    public void saveAll(Iterable<Endereco> iterable){
+        for (Endereco endereco: iterable) {
+            this.save(endereco);
+        }
     }
 
 }
