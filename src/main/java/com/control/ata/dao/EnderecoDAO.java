4aaca5704d8e6afe8f85fcc7f445d3211c4399ba
Theodoros16 <theodoros16@gamil.com>
2020-06-22 14:54:09 -0300

    EnderecoDAO agora possui a função de salver um EnderecoDTO por vez, não da para inserir listas de EnderecoDTO na função saveAll, mas da para salvar se inserir 1 EnderecoDTO na função save.
    EnderecoDTO agora possui construtores e getters e setters.

--- src/main/java/com/control/ata/dao/EnderecoDAO.java
+++ src/main/java/com/control/ata/dao/EnderecoDAO.java
@@ -1,13 +1,20 @@
 package com.control.ata.dao;
 
+import com.control.ata.dto.EnderecoDTO;
+import com.control.ata.model.endereco.Bairro;
+import com.control.ata.model.endereco.Cidade;
 import com.control.ata.model.endereco.Endereco;
-import com.control.ata.repository.endereco.*;
+import com.control.ata.repository.endereco.BairroRepository;
+import com.control.ata.repository.endereco.CidadeRepository;
+import com.control.ata.repository.endereco.EnderecoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EnderecoDAO {
 
+    @Autowired
+    private CidadeRepository cidadeRepository;
     @Autowired
     private BairroRepository bairroRepository;
     @Autowired
@@ -16,6 +23,19 @@ public class EnderecoDAO {
     public void save(Endereco endereco) {
         bairroRepository.save(endereco.getBairro());
         enderecoRepository.save(endereco);
+    }
+
+    public void save(EnderecoDTO enderecoDTO) {
+        if(bairroRepository.getBairroByNome(enderecoDTO.getBairro()) != null){
+            Bairro bairro = bairroRepository.getBairroByNome(enderecoDTO.getBairro());
+            bairroRepository.save(bairro);
+            enderecoRepository.save(new Endereco(enderecoDTO.getRua(), bairro));
+        }else {
+            Cidade cidade = cidadeRepository.getCidadeByNome(enderecoDTO.getCidade());
+            Bairro bairro = new Bairro(enderecoDTO.getBairro(), cidade);
+            bairroRepository.save(bairro);
+            enderecoRepository.save(new Endereco(enderecoDTO.getRua(), bairro));
+        }
     }
 
     public void saveAll(Iterable<Endereco> iterable){
--- src/main/java/com/control/ata/repository/endereco/CidadeRepository.java
+++ src/main/java/com/control/ata/repository/endereco/CidadeRepository.java
@@ -2,9 +2,14 @@ package com.control.ata.repository.endereco;
 
 import com.control.ata.model.endereco.Cidade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+    @Query("select c from Cidade c where c.nome = :cidade")
+    Cidade getCidadeByNome(@Param("cidade") String cidade);
 
 }
