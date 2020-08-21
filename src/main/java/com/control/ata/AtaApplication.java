24ea19a999f78b9c19ea54ebc018439833937c09
Chery-cake <theodoros16@gamil.com>
2020-08-21 18:58:18 -0300

    senha criptografada

--- src/main/java/com/control/ata/AtaApplication.java
+++ src/main/java/com/control/ata/AtaApplication.java
@@ -8,11 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @ImportResource(locations = "classpath:dwr-spring.xml")
 @SpringBootApplication
@@ -45,15 +41,4 @@ public class AtaApplication implements CommandLineRunner {
         return registrationBean;
     }
 
-}
-
-@Configuration
-@EnableWebSecurity
-class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity security) throws Exception
-    {
-        security.httpBasic().disable();
-    }
 }
